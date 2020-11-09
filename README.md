248339d8ee253f2cc6298fec4300908f12ff2857
Joseph Menesch <joe.menesch@gmail.com>
2020-11-09 18:51:15 +1000

    Added job url web scraper

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Data-Science-Job-Search
+# Data science job search
 A webscraper/processor for finding the most common required job skills
+
+## Files
+*job_url_ws.py* - Webscraper for getting urls from a base search (e.g. all urls from all pages of the search "Data scientist" on Seek).
+
+*job_info_ws.py* - For a job's given url, extract the text in the job description area.
+
+*text_processor.py* - Clean up a given list of text entries and search for key terms
+
+*main.py* - Using the above modules, find the highest frequency words (from a user-defined list) from a base search
---
+++ job_url_ws.py
@@ -0,0 +1,47 @@
+import urllib
+import urllib.request
+from html.parser import HTMLParser
+
+class SearchLinkParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self._recording = 0
+        self._in_url = 0
+        self._urls = []
+
+    def handle_starttag(self, tag, attr):
+        if tag == 'a' and ('class', '_2iNL7wI') in attr:
+            self._urls.extend([link for tag, link in attr if tag == 'href'])
+    
+    def get_urls(self):
+        return self._urls
+
+def get_job_urls(url):
+    urls_all = []
+    page_num = 1
+    
+    f = urllib.request.urlopen(url)
+    text = f.read()
+    f.close()
+    parser = SearchLinkParser()
+    parser.feed(str(text))
+    while((page_urls := parser.get_urls()) != []):
+        urls_all.extend(page_urls)
+        page_num += 1
+        url_next = ''.join([url, f'?page={page_num}'])
+        f = urllib.request.urlopen(url_next)
+        text = f.read()
+        f.close()
+        parser = SearchLinkParser()
+        parser.feed(str(text))
+        print(len(urls_all))
+
+    return urls_all
+
+def main():
+    search_url = "https://seek.com.au/data-science-jobs"
+    urls = get_job_urls(search_url)
+    print(urls)
+
+if __name__ == "__main__":
+    main()
